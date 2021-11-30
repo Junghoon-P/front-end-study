9488b19042b5508b739f7f00956f1652c1401a21
Junghoon-P <qkr0454@naver.com>
2021-11-30 23:33:19 +0900

    js study

--- 003. js/007_반복.js
+++ 003. js/007_반복.js
@@ -60,9 +60,45 @@ function maximum(value) {
 document.write(maximum(value)); //900
 document.write("<br>");
 
+//for
+let array = [10, 20, 30, 40, 50];
+let i = 0;
+
+for (let i = 0; i < array.length; ) {
+  document.write(array[i], "<br>");
+  i++;
+}
+
+array.forEach((e) => console.log(e * 3));
+
+for (var variable in array) {
+  document.write(array[variable], "<br>");
+}
+
+for (var variable of array) {
+  document.write(variable, "!! <br>");
+}
+
+//for of
+//합의 평균
+let array = [10, 20, 30, 40, 50];
+let i = 0;
+let sum = 0;
+
+for (let value of array) {
+  sum += value / array.length;
+}
+
 //Math.max.apply() 사용해서 최댓값 구하기
 var max2 = Math.max.apply(null, value);
 document.write(max2); //900
+
+//구구단
+for (var i = 2; i < 10; i++) {
+  for (var j = 1; j < 10; j++) {
+    document.write(`${i} X ${j} = ${i * j} <br>`);
+  }
+}
 
 //while
 let num = 0;
@@ -86,6 +122,17 @@ while (i < 10) {
   j = 1;
   i++;
 }
+//구구단
+let i = 2;
+let j = 1;
+while (i < 10) {
+  while (j < 10) {
+    console.log(`${i} X ${j} = ${i * j}`);
+    j++;
+  }
+  i++;
+  j = 1;
+}
 
 //
 let num = 0;
@@ -104,6 +151,21 @@ while (num < 11) {
   if (num > 5) {
     break;
   }
+}
+
+//
+let i = 2;
+let j = 1;
+while (i < 10) {
+  while (j < 10) {
+    console.log(`${i} X ${j} = ${i * j}`);
+    if (j == 5) {
+      break;
+    }
+    j++;
+  }
+  i++;
+  j = 1;
 }
 
 //리터럴 템플릿 99단 출력
@@ -210,5 +272,32 @@ for (let i = 0; i < 8; i++) {
   if (2 > 2) {
     temp = value[i + 1] - value[i];
     result = i;
+  }
+}
+
+// 무한루프
+// for (;;) { ... }
+// 무한루프
+// while (true) { ... }
+
+// outer라는 식별자가 붙은 레이블 for 문
+// 현업에서는 객체와 햇갈려서 잘 안쓰는 편
+// 할꺼면 function으로.
+outer: for (var i = 2; i < 10; i++) {
+  for (var j = 1; j < 10; j++) {
+    // i + j === 3이면 outer라는 식별자가 붙은 레이블 for 문을 탈출한다.
+    if (i === 5) break outer;
+    console.log(`${i} X ${j} = ${i * j}`);
+  }
+}
+
+// outer라는 식별자가 붙은 레이블 for 문
+// 현업에서는 객체와 햇갈려서 잘 안쓰는 편
+// 할꺼면 function으로.
+outer: for (var i = 2; i < 10; i++) {
+  for (var j = 1; j < 10; j++) {
+    // i + j === 3이면 outer라는 식별자가 붙은 레이블 for 문을 탈출한다.
+    if (i === 5) break outer;
+    console.log(`${i} X ${j} = ${i * j}`);
   }
 }
